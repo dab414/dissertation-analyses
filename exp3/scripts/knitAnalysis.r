c6362ed8e4ba9a6c5cf801dd766c422a6435f797
Dave <dab414@lehigh.edu>
2019-08-10 17:27:56 -0400

    Work on Exp 3 pipeline

--- exp3/scripts/knitAnalysis.r
+++ exp3/scripts/knitAnalysis.r
@@ -6,8 +6,10 @@ source('identitiesAndRejections/idCleansing.r')
 
 rmarkdown::render(input = 'preprocessing/exp3Preprocessing.Rmd', output_file = 'index.html')
 
-#rmarkdown::render(input = 'exploratory/rapidFire/exp2RapidFire.Rmd', output_file = 'index.html')
+rmarkdown::render(input = 'auxiliary/pracCued/exp3PracCued.Rmd', output_file = 'index.html')
+
+rmarkdown::render(input = 'exploratory/rapidFire/exp3RapidFire.Rmd', output_file = 'index.html')
 
 rmarkdown::render(input = 'demo/exp3Demo.Rmd', output_file = 'index.html')
 
-#rmarkdown::render(input = 'confirmatory/choice/exp2ConfirmatoryChoice.Rmd', output_file = 'index.html')
+rmarkdown::render(input = 'confirmatory/choice/exp3ConfirmatoryChoice.Rmd', output_file = 'index.html')
