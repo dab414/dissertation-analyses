db50f1bccdb5c1dc46c5affe97dda17ba8cbf735
Dave <dab414@lehigh.edu>
2019-08-13 08:51:34 -0400

    Various changes

--- commonFiles/scripts/demo/copyAndKnit.sh
+++ commonFiles/scripts/demo/copyAndKnit.sh
@@ -1,4 +1,4 @@
-cp demo.Rmd /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp2/scripts/demo/exp2demo.Rmd
+cp demo.Rmd /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp2/scripts/demo/exp2Demo.Rmd
 cp demo.Rmd /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp3/scripts/demo/exp3Demo.Rmd
 
 Rscript --vanilla /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/commonFiles/scripts/demo/knitAnalyses.r
